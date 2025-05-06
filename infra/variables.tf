62b214b4d1d00bda77078d41abaefaa8e64dd279
nkmelndz <nkmelndz@gmail.com>
2025-05-06 17:36:23 -0500

    aws9

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources"
-  default     = "us-east-1"
+  default     = "us-east-2"
 }
 variable "db_name" {
   description = "Nombre de la base de datos"
