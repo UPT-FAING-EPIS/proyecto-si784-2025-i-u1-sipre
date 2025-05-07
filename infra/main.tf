7eb1df8db797f46ac394756d89998d4e3c8ebca4
nkmelndz <nkmelndz@gmail.com>
2025-05-07 08:55:55 -0500

    aws14

--- infra/main.tf
+++ infra/main.tf
@@ -3,12 +3,12 @@ provider "aws" {
 }
 
 resource "aws_elastic_beanstalk_application" "php_app" {
-  name        = "markdown2video_app"
+  name        = "markdown2video-app"
   description = "PHP application deployed with Elastic Beanstalk"
 }
 
 resource "aws_elastic_beanstalk_environment" "php_app_env" {
-  name                = "markdown2video_env"
+  name                = "markdown2video-env"
   application         = aws_elastic_beanstalk_application.php_app.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.6.1 running PHP 8.1"
 
