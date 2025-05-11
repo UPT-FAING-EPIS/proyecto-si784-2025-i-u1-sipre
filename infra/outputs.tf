2d6d5496f192dd21d229ca58a53a332416a389f6
nkmelndz <nkmelndz@gmail.com>
2025-05-10 19:54:18 -0500

    output

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,3 +10,8 @@ output "db_port" {
   description = "Puerto de conexión del RDS"
   value       = aws_db_instance.mysql_db.port
 }
+
+output "s3_bucket_name" {
+  description = "Nombre del bucket"
+  value = aws_s3_bucket.app_bucket.bucket
+}
